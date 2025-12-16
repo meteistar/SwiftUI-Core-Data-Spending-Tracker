5e9bf0a0dd32b581cfe3609714e8ace7b7fd8b18
Mete Istar <gargamel_1987@hotmail.com>
2025-12-16 14:50:01 +0300

    feat: add Photo Picker with UIViewControllerRepresentable

--- SwiftUICoreDataSpendingTracker/Main/AddTransaction/AddTransactionForm.swift
+++ SwiftUICoreDataSpendingTracker/Main/AddTransaction/AddTransactionForm.swift
@@ -15,6 +15,9 @@ struct AddTransactionForm: View {
     @State private var amount = ""
     @State private var date = Date()
     
+    
+    @State private var shouldPresentPhotoPicker = false
+    
     var body: some View {
         NavigationView {
             Form {
@@ -29,11 +32,68 @@ struct AddTransactionForm: View {
                 
                 Section(header: Text("Photo/Receipt")){
                     Button{
-                        
-                    } label: { Text("Select Photo")}}
+                        shouldPresentPhotoPicker.toggle()
+                    } label: {
+                        Text("Select Photo")
+                    }
+                    .fullScreenCover(isPresented: $shouldPresentPhotoPicker, content: { PhotoPickerView(photoData: $photoData) })
+                    
+                    if let data = self.photoData, let image = UIImage.init(data: data) {
+                        Image(uiImage: image)
+                            .resizable()
+                            .scaledToFill()
+                            
+                    }
+                }
+                
             }
             .navigationBarTitle("Add Transaction")
             .navigationBarItems(leading: cancelButton, trailing: saveButton)
+        }
+    }
+    
+    
+    @State private var photoData: Data?
+    
+    struct PhotoPickerView: UIViewControllerRepresentable {
+        
+        @Binding var photoData: Data?
+        
+        func makeCoordinator() -> Coordinator {
+            return Coordinator(parent: self)
+        }
+        
+        class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+            
+            private let parent: PhotoPickerView
+            
+            init(parent: PhotoPickerView) {
+                self.parent = parent
+            }
+            
+            func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+                
+                let image = info[.originalImage] as? UIImage
+                let imageData = image?.jpegData(compressionQuality: 1)
+                self.parent.photoData = imageData
+                
+                picker.dismiss(animated: true)
+                
+            }
+            
+            func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+                picker.dismiss(animated: true)
+            }
+            
+        }
+        func makeUIViewController(context: Context) -> UIViewController {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = context.coordinator
+            return imagePicker
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+            
         }
     }
     
